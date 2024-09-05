66ff849f9d5fbce4cc26b7365fcd0946ec4dab70
Lexi Mattick <lexi.mattick@neuralink.com>
2024-09-05 09:27:43 -0700

    Fix build script

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -7,7 +7,7 @@ source "$(dirname "$0")/_include.sh"
 clean_artifacts
 docker_build_linux linux/amd64 x86_64-linux linux-x86_64
 echo
-docker_build_linux linux/arm64 aarch64-linux linux-arm64
+docker_build_linux linux/arm64 aarch64-linux linux-aarch64
 echo
 echo "✅ Done! Artifacts are in $artifacts_dir"
 echo
